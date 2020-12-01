fc3bf440d7d1308828bab13edb3b3a56956412a2
JJ Vega <jjvega@jjs-macbook-pro.lan>
2020-12-01 12:55:27 -0500

    Added class skeletons to game.js

--- game.js
+++ game.js
@@ -6,8 +6,51 @@
 // Gesture arrays to hold all available gestures - incorporate inheritance here as well
 // Compare gestures to find winner of a round
 // Minimum of "best of three" rounds to determine winner of game
+// use console to play the game (using logs, prompts, alerts)
 
 // Classes: 
-// Game - runs all of the game logic. Instantiate all of the Player objects in the constructor. 
+// Game - runs all of the game logic. Instantiate all of the Player objects in the constructor. runGame method that is called at end of script.
+// Game cont. - displayRules shows all the rules upon running the game
 // Player(parent) => Human(child), Computer(child). Has an array of Gestures. Can ChooseGesture and display gesture options.
 // Gesture(parent) => Rock(child), Paper(child), Scissors(child), Lizard(child), Spock(child). Have an array that specifies "loses to" for each
+
+
+class Game{
+
+}
+
+class Player{ // parent
+
+}
+
+class Human extends Player{
+
+}
+
+class Computer extends Player{
+
+}
+
+class Gesture{ // parent
+
+}
+
+class Rock extends Gesture{
+
+}
+
+class Paper extends Gesture{
+    
+}
+
+class Scissors extends Gesture{
+    
+}
+
+class Lizard extends Gesture{
+    
+}
+
+class Spock extends Gesture{
+    
+}
